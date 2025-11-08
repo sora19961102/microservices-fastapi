adacb6aceddc4c3b63959b3ab6608f8ada261b5e
sora19961102 <sora11023539@gmail.com>
2025-11-08 13:04:53 -0800

    Add: Introduce .gitignore files and .env.example for payment, and enhance main.py with Redis integration and order management

--- payment/main.py
+++ payment/main.py
@@ -1,15 +1,63 @@
 from typing import Union
+import os
+from dotenv import load_dotenv
 
-from fastapi import FastAPI
+from fastapi import FastAPI, BackgroundTasks
+from fastapi.middleware.cors import CORSMiddleware
+
+from redis_om import get_redis_connection, HashModel
+from starlette.requests import Request
+
+import requests
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+redis = get_redis_connection(
+    host=os.getenv("REDIS_HOST"),
+    port=int(os.getenv("REDIS_PORT")),
+    password=os.getenv("REDIS_PASSWORD"),
+    decode_responses=True,
+)
 
+class Order(HashModel):
+    product_id: str
+    price: float
+    fee: float
+    total: float
+    quantity: int
+    status: str # pending, completed, refunded
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+    class Meta:
+        database = redis
+
+@app.post("/orders")
+async def create(request: Request, background_tasks: BackgroundTasks):
+    body = await request.json()
+    req = requests.get(f"http://localhost:8000/products/{body['id']}")
+    product = req.json()
+    order = Order(
+        product_id=body['id'],
+        price=product['price'],
+        fee=0.2 * product['price'],
+        total=1.2 * product['price'],
+        quantity=1,
+        status='pending'
+    )
+    order.save()
+    background_tasks.add_task(order_completed, order)
+
+    return order
+
+def order_completed(order: Order):
+    order.status = 'completed'
+    order.save()
